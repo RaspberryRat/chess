7f7ead9c2b2b3b7704645ed29530f41a789342d4
G <mail@grahamirvine.com>
2022-07-26 11:31:28 -0600

    added numbers and letters

--- test_board.rb
+++ test_board.rb
@@ -19,6 +19,7 @@ empty_row_green = "#{green_square}   #{white_square}   " * 4 + "\u001b[0m\n"
 empty_row_white = "#{white_square}   #{green_square}   " * 4 + "\u001b[0m\n"
 # row 8 ROW
 puts "\n" * 5
+print "8 "
 print "#{green_square} #{black_rook} "
 print "#{white_square} #{black_knight} "
 print "#{green_square} #{black_bishop} "
@@ -30,16 +31,25 @@ print "#{white_square} #{black_rook} "
 #end of line
 puts "\u001b[0m\n"
 #line 7
+print "7 "
 print "#{white_square} #{black_pawn} #{green_square} #{black_pawn} " * 4
 #end of line
 puts "\u001b[0m\n"
-#line 6 to 3
-print "#{empty_row_green}#{empty_row_white}" * 2
+#line 6
+print "6 #{empty_row_green}"
+#line 5
+print "5 #{empty_row_white}"
+# line 4
+print "4 #{empty_row_green}"
+# line 3
+print "3 #{empty_row_white}"
 # line 2 
+print "2 "
 print "#{green_square} #{white_pawn} #{white_square} #{white_pawn} " * 4
 #end of line
 puts "\u001b[0m\n"
 # line 1
+print "1 "
 print "#{white_square} #{white_rook} "
 print "#{green_square} #{white_knight} "
 print "#{white_square} #{white_bishop} "
@@ -50,4 +60,5 @@ print "#{white_square} #{white_knight} "
 print "#{green_square} #{white_rook} "
 #end of line
 puts "\u001b[0m\n"
+print "   A  B  C  D  E  F  G  H"
 puts "\n" * 5
