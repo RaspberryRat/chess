d03713746f26aefcffc728b7f89634983e8ee41e
G <mail@grahamirvine.com>
2022-07-25 18:26:36 -0600

    created variables for pieces with unicode and working on print board

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 require_relative 'main'
 
 # playing board for pieces
 class Board
   attr_reader :board
-  
+
   def initialize(board = build_board)
     @board = board
   end
@@ -13,4 +14,13 @@ class Board
     (1..8).to_a.repeated_permutation(2) { | arr| board_array << arr }
     board_array
   end
+
+  # TODO this isn't done
+  def print_board
+    single_pair = "\u001b[4m\u001b[48;5;107m  \u001b[48;5;231m  "
+    one_line = single_pair * 4 + "\u001b[0m\n"
+    output = one_line * 8
+    puts output
+  end
 end
+
---
+++ test_board.rb
@@ -0,0 +1,27 @@
+white_king = "\u2654"
+white_queen = "\u2655"
+white_rook = "\u2656"
+white_bishop = "\u2657"
+white_knight = "\u2658"
+white_pawn = "\u2659"
+
+black_king = "\u265A"
+black_queen = "\u265B"
+black_rook = "\u265C"
+black_bishop = "\u265D"
+black_knight = "\u265E"
+black_pawn = "\u265F"
+
+puts white_king
+puts white_queen
+puts white_rook
+puts white_bishop
+puts white_knight
+puts white_pawn
+
+puts black_king
+puts black_queen
+puts black_rook
+puts black_bishop
+puts black_knight
+puts black_pawn
