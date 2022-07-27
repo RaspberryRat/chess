93e31624b1bc87745f0c349e1861cd33d5466856
G <mail@grahamirvine.com>
2022-07-27 11:12:59 -0600

    moved the board array into a Struct class

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
+require 'pry-byebug'
 require_relative 'main'
 require_relative 'piece_module'
 include PieceVariables
 
 # playing board for pieces
 class Board
-  attr_reader :board
+  attr_accessor :board
 
-  def initialize(board = build_board)
+  def initialize(board = positions(build_board))
     @board = board
   end
 
@@ -17,20 +18,25 @@ class Board
     board_array
   end
 
+  BoardLocations = Struct.new(:x_position, :y_position, :game_piece)
+  def positions(location)
+    location.map { |cell| BoardLocations.new(cell[0], cell[1], cell[2]) }
+  end
+
   def print_board
     chess_board = ''
     board.reverse.map do |pos|
-      chess_board += "#{pos[0]} " if pos[1] == 8
+      chess_board += "#{pos.x_position} " if pos.y_position == 8
 
-      first_value = pos[0].even?
-      if pos[1].even? == first_value
+      first_value = pos.x_position.even?
+      if pos.y_position.even? == first_value
         chess_board += "#{GREEN_SQUARE}"
-        chess_board += pos[2].nil? ? "#{EMPTY_SPACE} " : " #{pos[2]} "
+        chess_board += pos.game_piece.nil? ? "#{EMPTY_SPACE} " : " #{pos.game_piece} "
       else
         chess_board += "#{WHITE_SQUARE}"
-        chess_board += pos[2].nil? ? "#{EMPTY_SPACE} " : " #{pos[2]} "
+        chess_board += pos.game_piece.nil? ? "#{EMPTY_SPACE} " : " #{pos.game_piece} "
       end
-      chess_board += NEW_LINE if pos[1] == 1
+      chess_board += NEW_LINE if pos.y_position == 1
     end
     chess_board += "   A  B  C  D  E  F  G  H\n"
     print chess_board
