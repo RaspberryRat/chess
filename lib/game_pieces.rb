cc115b233e9ee203a3bbe0431ac0a752f9b95f94
G <mail@grahamirvine.com>
2022-07-27 17:30:32 -0600

    added methods and tests for setting start location

--- lib/game_pieces.rb
+++ lib/game_pieces.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'piece_module'
+require 'pry-byebug'
 
 include PieceVariables
 
@@ -31,106 +32,181 @@ class GamePiece
       BlackKing
     when 25..32
       BlackPawn
-    end.new
+    end.new(count)
 
   end
   
-  attr_reader :piece
+  attr_reader :piece, :location, :piece_number
 
-  def initialize(location = nil)
+  def initialize(piece_number, location = set_start_location)
     @piece = nil
-    @location = nil
+    @piece_number = piece_number
+    @location = location
+  end
+
+  def set_start_location
+    false
   end
 end
 
 class Pawn < GamePiece
-    def initialize(location = nil)
-      super(location)
+    def initialize(piece_number, location = set_start_location)
+      super(piece_number, location)
       @piece = WHITE_PAWN
+
+    def set_start_location
+      location = case piece_number
+      when 9
+        [2, 1]
+      when 10
+        [2, 2]
+      when 11
+        [2, 3]
+      when 12
+        [2, 4]
+      when 13
+        [2, 5]
+      when 14
+        [2, 6]
+      when 15
+        [2, 7]
+      when 16
+        [2, 8]
+      end
+    end
   end
+
 
 end
 
 class BlackPawn < Pawn
-  def initialize(location = nil)
-      super(location)
+  def initialize(piece_number, location = set_start_location)
+      super(piece_number, location)
       @piece = BLACK_PAWN
   end
 
 end
 
 class Rook < GamePiece
-  def initialize(location = nil)
-    super(location)
+  def initialize(piece_number, location = set_start_location)
+
+    super(piece_number, location)
     @piece = WHITE_ROOK
   end
 
+  def set_start_location
+    return location = [1, 1] if piece_number == 1
+
+    location = [1, 8]
+  end
 end
 
-class BlackRook < Rook
-  def initialize(location = nil)
-    super(location)
+class BlackRook < GamePiece
+  def initialize(piece_number, location = set_start_location)
+    super(piece_number, location)
+
     @piece = BLACK_ROOK
   end
 
+  def set_start_location
+    return location = [8, 1] if piece_number == 17
+
+    location = [8, 8]
+  end
 end
 
 class Knight < GamePiece
-  def initialize(location = nil)
-    super(location)
+  def initialize(piece_number, location = set_start_location)
+    super(piece_number, location)
     @piece = WHITE_KNIGHT
   end
 
+  def set_start_location
+    return location = [1, 2] if piece_number == 2
+
+    location = [1, 7]
+  end
 end
 
 class BlackKnight < Knight
-  def initialize(location = nil)
-    super(location)
+  def initialize(piece_number, location = set_start_location)
+    super(piece_number, location)
     @piece = BLACK_KNIGHT
   end
-  
+
+  def set_start_location
+    return location = [8, 2] if piece_number == 18
+
+    location = [8, 7]
+  end
 end
 
 class Bishop < GamePiece
-  def initialize(location = nil)
-    super(location)
+  def initialize(piece_number, location = set_start_location)
+    super(piece_number, location)
     @piece = WHITE_BISHOP
+  end
+
+  def set_start_location
+    return location = [1, 3] if piece_number == 3
+
+    location = [1, 6]
   end
 end
 
 class BlackBishop < Bishop
-  def initialize(location = nil)
-    super(location)
+  def initialize(piece_number, location = set_start_location)
+    super(piece_number, location)
     @piece = BLACK_BISHOP
   end
 
+  def set_start_location
+    return location = [8, 3] if piece_number == 19
+
+    location = [8, 6]
+  end
 end
 
 class Queen < GamePiece
-  def initialize(location = nil)
-    super(location)
+  def initialize(piece_number, location = set_start_location)
+    super(piece_number, location)
     @piece = WHITE_QUEEN
   end
 
+  def set_start_location
+    location = [1, 4]
+  end
 end
 
 class BlackQueen < Queen
-  def initialize(location = nil)
-    super(location)
+  def initialize(piece_number, location = set_start_location)
+    super(piece_number, location)
     @piece = BLACK_QUEEN
+  end
+
+  def set_start_location
+    location = [8, 4]
   end
 end
 
 class King < GamePiece
-  def initialize(location = nil)
-    super(location)
+  def initialize(piece_number, location = set_start_location)
+    super(piece_number, location)
     @piece = WHITE_KING
+  end
+
+  def set_start_location
+    location = [1, 5]
   end
 end
 
 class BlackKing < King
-  def initialize(location = nil)
-    super(location)
+  def initialize(piece_number, location = set_start_location)
+    super(piece_number, location)
     @piece = BLACK_KING
+  end
+
+  def set_start_location
+    location = [8, 5]
   end
 end
--- spec/game_pieces_spec.rb
+++ spec/game_pieces_spec.rb
@@ -117,5 +117,352 @@ describe GamePiece do
       end
     end
   end
-  
+end
+
+describe Rook do
+  describe "#set_start_location" do
+
+    context 'when first rook is created' do
+      subject(:first_rook) { described_class.new(count) }
+      let(:count)  { 1 }
+      it 'has location of [1, 1]' do
+        location = first_rook.set_start_location
+        expect(location).to eq([1, 1])
+      end
+    end
+
+    context 'when second rook is created' do
+      subject(:second_rook) { described_class.new(count) }
+      let(:count)  { 8 }
+      it 'has location of [1, 8]' do
+        location = second_rook.set_start_location
+        expect(location).to eq([1, 8])
+      end
+    end
+  end
+end
+
+describe Knight do
+  describe "#set_start_location" do
+
+    context 'when first knight is created' do
+      subject(:first_knight) { described_class.new(count) }
+      let(:count)  { 2 }
+      it 'has location of [1, 2]' do
+        location = first_knight.set_start_location
+        expect(location).to eq([1, 2])
+      end
+    end
+
+    context 'when second knight is created' do
+      subject(:second_knight) { described_class.new(count) }
+      let(:count)  { 7 }
+      it 'has location of [1, 7]' do
+        location = second_knight.set_start_location
+        expect(location).to eq([1, 7])
+      end
+    end
+  end
+end
+
+describe Bishop do
+  describe "#set_start_location" do
+
+    context 'when first bishop is created' do
+      subject(:first_bishop) { described_class.new(count) }
+      let(:count)  { 3 }
+      it 'has location of [1, 3]' do
+        location = first_bishop.set_start_location
+        expect(location).to eq([1, 3])
+      end
+    end
+
+    context 'when second bishop is created' do
+      subject(:second_bishop) { described_class.new(count) }
+      let(:count)  { 6 }
+      it 'has location of [1, 6]' do
+        location = second_bishop.set_start_location
+        expect(location).to eq([1, 6])
+      end
+    end
+  end
+end
+
+describe Queen do
+  describe "#set_start_location" do
+
+    context 'when white queen is created' do
+      subject(:white_queen) { described_class.new(count) }
+      let(:count)  { 4 }
+      it 'has location of [1, 4]' do
+        location = white_queen.set_start_location
+        expect(location).to eq([1, 4])
+      end
+    end
+  end
+end
+
+describe King do
+  describe "#set_start_location" do
+
+    context 'when white king is created' do
+      subject(:white_king) { described_class.new(count) }
+      let(:count)  { 5 }
+      it 'has location of [1, 5]' do
+        location = white_king.set_start_location
+        expect(location).to eq([1, 5])
+      end
+    end
+  end
+end
+
+describe Pawn do
+  describe "#set_start_location" do
+
+    context 'when first white pawn is created' do
+      subject(:first_pawn) { described_class.new(count) }
+      let(:count)  { 9 }
+      it 'has location of [2, 1]' do
+        location = first_pawn.set_start_location
+        expect(location).to eq([2, 1])
+      end
+    end
+
+    context 'when second white pawn is created' do
+      subject(:second_pawn) { described_class.new(count) }
+      let(:count)  { 10 }
+      it 'has location of [2, 2]' do
+        location = second_pawn.set_start_location
+        expect(location).to eq([2, 2])
+      end
+    end
+
+    context 'when third white pawn is created' do
+      subject(:third_pawn) { described_class.new(count) }
+      let(:count)  { 11 }
+      it 'has location of [2, 3]' do
+        location = third_pawn.set_start_location
+        expect(location).to eq([2, 3])
+      end
+    end
+
+    context 'when fourth white pawn is created' do
+      subject(:fourth_pawn) { described_class.new(count) }
+      let(:count)  { 12 }
+      it 'has location of [2, 4]' do
+        location = fourth_pawn.set_start_location
+        expect(location).to eq([2, 4])
+      end
+    end
+
+    context 'when fifth white pawn is created' do
+      subject(:fifth_pawn) { described_class.new(count) }
+      let(:count)  { 13 }
+      it 'has location of [2, 5]' do
+        location = fifth_pawn.set_start_location
+        expect(location).to eq([2, 5])
+      end
+    end
+
+    context 'when sixth white pawn is created' do
+      subject(:sixth_pawn) { described_class.new(count) }
+      let(:count)  { 14 }
+      it 'has location of [2, 6]' do
+        location = sixth_pawn.set_start_location
+        expect(location).to eq([2, 6])
+      end
+    end
+
+    context 'when seventh white pawn is created' do
+      subject(:seventh_pawn) { described_class.new(count) }
+      let(:count)  { 15 }
+      it 'has location of [2, 7]' do
+        location = seventh_pawn.set_start_location
+        expect(location).to eq([2, 7])
+      end
+    end
+
+    context 'when eight white pawn is created' do
+      subject(:eight_pawn) { described_class.new(count) }
+      let(:count)  { 16 }
+      it 'has location of [2, 8]' do
+        location = eight_pawn.set_start_location
+        expect(location).to eq([2, 8])
+      end
+    end
+  end
+end
+
+describe BlackRook do
+  describe "#set_start_location" do
+
+    context 'when first black rook is created' do
+      subject(:first_rook) { described_class.new(count) }
+      let(:count)  { 17 }
+      it 'has location of [8, 1]' do
+        location = first_rook.set_start_location
+        expect(location).to eq([8, 1])
+      end
+    end
+
+    context 'when second rook is created' do
+      subject(:second_rook) { described_class.new(count) }
+      let(:count)  { 24 }
+      it 'has location of [8, 8]' do
+        location = second_rook.set_start_location
+        expect(location).to eq([8, 8])
+      end
+    end
+  end
+end
+
+describe BlackKnight do
+  describe "#set_start_location" do
+
+    context 'when first black knight is created' do
+      subject(:first_knight) { described_class.new(count) }
+      let(:count)  { 18 }
+      it 'has location of [8, 2]' do
+        location = first_knight.set_start_location
+        expect(location).to eq([8, 2])
+      end
+    end
+
+    context 'when second black knight is created' do
+      subject(:second_knight) { described_class.new(count) }
+      let(:count)  { 23 }
+      it 'has location of [8, 7]' do
+        location = second_knight.set_start_location
+        expect(location).to eq([8, 7])
+      end
+    end
+  end
+end
+
+describe BlackBishop do
+  describe "#set_start_location" do
+
+    context 'when first black bishop is created' do
+      subject(:first_bishop) { described_class.new(count) }
+      let(:count)  { 19 }
+      it 'has location of [8, 3]' do
+        location = first_bishop.set_start_location
+        expect(location).to eq([8, 3])
+      end
+    end
+
+    context 'when second black bishop is created' do
+      subject(:second_bishop) { described_class.new(count) }
+      let(:count)  { 22 }
+      it 'has location of [8, 6]' do
+        location = second_bishop.set_start_location
+        expect(location).to eq([8, 6])
+      end
+    end
+  end
+end
+
+describe BlackQueen do
+  describe "#set_start_location" do
+
+    context 'when black queen is created' do
+      subject(:black_queen) { described_class.new(count) }
+      let(:count)  { 21 }
+      it 'has location of [8, 4]' do
+        location = black_queen.set_start_location
+        expect(location).to eq([8, 4])
+      end
+    end
+  end
+end
+
+describe BlackKing do
+  describe "#set_start_location" do
+
+    context 'when black king is created' do
+      subject(:black_king) { described_class.new(count) }
+      let(:count)  { 21 }
+      it 'has location of [8, 5]' do
+        location = black_king.set_start_location
+        expect(location).to eq([8, 5])
+      end
+    end
+  end
+end
+
+describe Pawn do
+  describe "#set_start_location" do
+
+    context 'when first white pawn is created' do
+      subject(:first_pawn) { described_class.new(count) }
+      let(:count)  { 9 }
+      it 'has location of [2, 1]' do
+        location = first_pawn.set_start_location
+        expect(location).to eq([2, 1])
+      end
+    end
+
+    context 'when second white pawn is created' do
+      subject(:second_pawn) { described_class.new(count) }
+      let(:count)  { 10 }
+      it 'has location of [2, 2]' do
+        location = second_pawn.set_start_location
+        expect(location).to eq([2, 2])
+      end
+    end
+
+    context 'when third white pawn is created' do
+      subject(:third_pawn) { described_class.new(count) }
+      let(:count)  { 11 }
+      it 'has location of [2, 3]' do
+        location = third_pawn.set_start_location
+        expect(location).to eq([2, 3])
+      end
+    end
+
+    context 'when fourth white pawn is created' do
+      subject(:fourth_pawn) { described_class.new(count) }
+      let(:count)  { 12 }
+      it 'has location of [2, 4]' do
+        location = fourth_pawn.set_start_location
+        expect(location).to eq([2, 4])
+      end
+    end
+
+    context 'when fifth white pawn is created' do
+      subject(:fifth_pawn) { described_class.new(count) }
+      let(:count)  { 13 }
+      it 'has location of [2, 5]' do
+        location = fifth_pawn.set_start_location
+        expect(location).to eq([2, 5])
+      end
+    end
+
+    context 'when sixth white pawn is created' do
+      subject(:sixth_pawn) { described_class.new(count) }
+      let(:count)  { 14 }
+      it 'has location of [2, 6]' do
+        location = sixth_pawn.set_start_location
+        expect(location).to eq([2, 6])
+      end
+    end
+
+    context 'when seventh white pawn is created' do
+      subject(:seventh_pawn) { described_class.new(count) }
+      let(:count)  { 15 }
+      it 'has location of [2, 7]' do
+        location = seventh_pawn.set_start_location
+        expect(location).to eq([2, 7])
+      end
+    end
+
+    context 'when eight white pawn is created' do
+      subject(:eight_pawn) { described_class.new(count) }
+      let(:count)  { 16 }
+      it 'has location of [2, 8]' do
+        location = eight_pawn.set_start_location
+        expect(location).to eq([2, 8])
+      end
+    end
+  end
 end
