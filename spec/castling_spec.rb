7d0a383dd67d5aac60f6ab89d66fc94598a2941b
G <mail@grahamirvine.com>
2023-04-16 17:59:16 -0600

    added tests for black player

--- spec/castling_spec.rb
+++ spec/castling_spec.rb
@@ -16,7 +16,7 @@ describe Castling do
       subject(:kingside_castle) { described_class.new(board) }
       let(:board) { "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3KR1 w Qkq" }
 
-      it "returns true" do
+      it "returns false" do
         result = kingside_castle.castling_available?
         expect(result).to be(false)
       end
@@ -26,7 +26,37 @@ describe Castling do
       subject(:kingside_castle) { described_class.new(board) }
       let(:board) { "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3KB1R w KQkq" }
 
+      it "returns false" do
+        result = kingside_castle.castling_available?
+        expect(result).to be(false)
+      end
+    end
+
+    context "when black player has kingside castling" do
+      subject(:kingside_castle) { described_class.new(board) }
+      let(:board) { "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R b KQkq" }
+
       it "returns true" do
+        result = kingside_castle.castling_available?
+        expect(result).to be(true)
+      end
+    end
+
+    context "when black player has no kingside castling" do
+      subject(:kingside_castle) { described_class.new(board) }
+      let(:board) { "r3kR1/pppppppp/8/8/8/8/PPPPPPPP/R3KR1 b Qq" }
+
+      it "returns false" do
+        result = kingside_castle.castling_available?
+        expect(result).to be(false)
+      end
+    end
+
+    context "when black player has no kingside castling" do
+      subject(:kingside_castle) { described_class.new(board) }
+      let(:board) { "r3kb1r/pppppppp/8/8/8/8/PPPPPPPP/R3KB1R b KQkq" }
+
+      it "returns false" do
         result = kingside_castle.castling_available?
         expect(result).to be(false)
       end
