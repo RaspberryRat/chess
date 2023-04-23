f72093a48ccea0d66bbd0e2d1b9f39f74c53a677
G <mail@grahamirvine.com>
2023-04-23 09:07:32 -0600

    updated path location for board square test

--- spec/board_square_spec.rb
+++ spec/board_square_spec.rb
@@ -1,21 +1,18 @@
-require_relative '../lib/board_square'
+require_relative "../lib/board_squares/board_square"
 
 describe BoardSquare do
-  describe '.for' do
-    context 'when notation is line break' do
-
+  describe ".for" do
+    context "when notation is line break" do
       subject(:new_square) { described_class }
-      let(:new_line) { '/' }
-      let(:color) { 'color'}
+      let(:new_line) { "/" }
+      let(:color) { "color" }
 
-  
-      it 'is BoardSquare' do
+      it "is BoardSquare" do
         returned_object = new_square.for(new_line, color)
         expect(returned_object).to be_an_instance_of(BoardSquare)
       end
 
-      
-      it 'returns a new line' do
+      it "returns a new line" do
         next_line = "\u001b[0m\n"
         output = new_square.for(new_line, color).to_s
         expect(output).to eq(next_line)
